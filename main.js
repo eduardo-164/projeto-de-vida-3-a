482c78803608d77802abb62daaffbf9eaf2650f9
eduardo-164 <128702253+eduardo-164@users.noreply.github.com>
2024-04-17 16:27:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const botoes = document.querySelector(".botao
+const botoes = document.querySelector(".botao");
 
 for(let i =0;i <botoes. length;i++){
-    botoes[i] onclick = function() 
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+}
